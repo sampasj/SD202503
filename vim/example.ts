129c521396af59a5f5da3221855394f0485f0dd7
sampasj <saku@XPS13>
2025-03-23 19:17:15 +0900

    mod example

--- vim/example.ts
+++ vim/example.ts
@@ -1,5 +1,5 @@
-function processOrder (order: Order) {
-    validateOrder (order);
+function processOrder(order: Order) {
+    validateOrder(order);
     const quantity = order.quantity;
     if (!isValidQuantity(quantity)) {
       throw new Error('Invalid quantity');
